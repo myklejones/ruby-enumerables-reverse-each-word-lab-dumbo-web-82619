d9780d4f575f6bc75c45da51ebcdc4754bbe8799
Mykle jones <mykle.j91@gmail.com>
2019-08-17 07:17:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,13 @@ new_word.each do |string|
  end
 array.join(" ")
 end 
+
+def reverse_each_word (string)
+new_word=[]
+new_word=string.split(" ")
+array=[]
+new_word.collect do |string|
+  array<< string.reverse
+ end
+array.join(" ")
+end 
